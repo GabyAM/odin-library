15a8f962e47ebf75f6c56d7a315558b13b64b644
GabyAM <gdgabyam@gmail.com>
2023-11-02 18:59:57 -0300

    added feature to add books to the library
    added the form and when submitted,  a Book instance is created and added to the books array. There´s also a function to display the books inside the array on a table

--- index.js
+++ index.js
@@ -10,3 +10,46 @@ function Book (title, author, pages, read) {
         return `${title} by ${author}, ${pages} pages, ${read ? 'read' : 'not read yet'}`
     }
 } 
+
+function renderLibrary() {
+    books.forEach(book => {
+        const $tableRow = document.createElement('tr');
+        const $titleCell = document.createElement('td');
+        $titleCell.textContent = book.title;
+        const $authorCell = document.createElement('td');
+        $authorCell.textContent = book.author
+        const $pagesCell = document.createElement('td');
+        $pagesCell.textContent = book.pages;
+        const $readCell = document.createElement('td');
+        $readCell.textContent = book.read;
+        $tableRow.appendChild($titleCell);
+        $tableRow.appendChild($authorCell);
+        $tableRow.appendChild($pagesCell);
+        $tableRow.appendChild($readCell);
+        document.querySelector('tbody').appendChild($tableRow);
+    })
+}
+
+function addBookToLibrary(book) {
+    books.push(book);
+    renderLibrary();
+}
+
+const $form = document.querySelector('form');
+$form.addEventListener('submit', (event) => {
+    const formData = new FormData($form);
+    const title = formData.get('title');
+    const author = formData.get('author');
+    const pages = formData.get('pages');
+    const read = formData.get('read');
+    const book = new Book(title, author, pages, read);
+    addBookToLibrary(book);
+    $form.reset();
+    event.preventDefault();
+})
+
+const $button = document.querySelector('.open-sidebar-button');
+$button.addEventListener('click', () => {
+    document.querySelector('aside').style.width = '50%';
+    $button.disabled = true;
+})
